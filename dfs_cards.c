0eeb81661103227ceebb79749a6349611a6be2cc
mzq <1845598311@qq.com>
2021-09-11 14:59:37 +0800

    打框架

--- dfs_cards.c
+++ dfs_cards.c
@@ -2,9 +2,14 @@
 输出数的全排序，用将扑克牌放进箱子里进行类比
 The total sorting of the output number, analogy with putting the playing cards in the box
 */
-int dfs_cards_numers[100];
+int dfs_cards_digit;
+int dfs_cards_book[100];
+int dfs_cards_a[100];
+int dfs_cards_;
 
 void dfs_cards() {
 
 	return;
 }
+
+
